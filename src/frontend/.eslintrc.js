269ed6b6061ff538ace4ae1b3d579fa65fa083ed
ivankprod <houseprotector@mail.ru>
2021-10-17 14:58:33 +0300

    ESLint integration completed

---
+++ src/frontend/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	env: {
+		es6: true
+	},
+
+	parserOptions: {
+		sourceType: "module",
+		ecmaVersion: 2017,
+		ecmaFeatures: {
+			spread: true,
+			restParams: true,
+			jsx: false
+		}
+	}
+}
