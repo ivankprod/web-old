6c3f35c9beabf2986247c790a36ed415a53db364
ivankprod <houseprotector@mail.ru>
2021-10-21 16:10:31 +0300

    Docker integration fixes

--- compose.sh
+++ compose.sh
@@ -1,4 +1,17 @@
-. ./prod.env
+echo "Starting composing webapp..."
+
+mode="prod"
+if [[ "$1" == "dev" ]]; then
+	mode=$1
+elif [[ "$1" == "prod" ]] || [[ "$1" == "" ]]; then
+	mode="prod"
+else
+	echo "Error argument parsing"; exit
+fi
+
+echo "MODE: $mode"
+
+. ./$mode.env
 
 set -a
 DB_TARANTOOL_PORT=$DB_TARANTOOL_PORT
@@ -7,3 +20,5 @@ DB_TARANTOOL_PASSWORD=$DB_TARANTOOL_PASSWORD
 set +a
 
 docker-compose up -d
+
+echo "Composing webapp done!"
