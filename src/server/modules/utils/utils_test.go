70e5fe6dc28e7aed3faa20bcca14b1372003158e
ivankprod <houseprotector@mail.ru>
2021-10-25 05:23:46 +0300

    Server: testing for utils module

--- src/server/modules/utils/utils_test.go
+++ src/server/modules/utils/utils_test.go
@@ -18,7 +18,7 @@ func TestSitemapPath_addChild(t *testing.T) {
 		want bool
 	}{
 		{
-			name: "Adding one child to SitemapPath",
+			name: "Adding one child to SitemapPath child",
 			p: &SitemapPath{
 				ID:       1,
 				Title:    "Some title",
@@ -45,6 +45,73 @@ func TestSitemapPath_addChild(t *testing.T) {
 				},
 			},
 			want: true,
+		},
+		{
+			name: "Adding one child to SitemapPath child's child",
+			p: &SitemapPath{
+				ID:       1,
+				Title:    "Some title",
+				Path:     "/some/path",
+				Priority: 1,
+				Children: []SitemapPath{
+					{
+						ID:       2,
+						ParentID: 1,
+						Title:    "First child title",
+						Path:     "/child1/path",
+						Priority: 0.8,
+						Children: []SitemapPath{
+							{
+								ID:       3,
+								ParentID: 2,
+								Title:    "Second child title",
+								Path:     "/child2/path",
+								Priority: 0.6,
+							},
+						},
+					},
+				},
+			},
+			args: args{
+				parentID: 3,
+				child: &SitemapPath{
+					ID:       4,
+					ParentID: 3,
+					Title:    "Third child title",
+					Path:     "/child3/path",
+					Priority: 0.4,
+				},
+			},
+			want: true,
+		},
+		{
+			name: "Error on adding one child to SitemapPath child",
+			p: &SitemapPath{
+				ID:       1,
+				Title:    "Some title",
+				Path:     "/some/path",
+				Priority: 1,
+				Children: []SitemapPath{
+					{
+						ID:       2,
+						ParentID: 1,
+						Title:    "First child title",
+						Path:     "/child1/path",
+						Priority: 0.8,
+					},
+				},
+			},
+			args: args{
+				parentID: 3,
+				child: &SitemapPath{
+					ID:       3,
+					ParentID: 3,
+					Title:    "Second child title",
+					Path:     "/child2/path",
+					Priority: 0.8,
+				},
+			},
+			want: false,
 		},
 	}
 
@@ -133,6 +200,34 @@ func TestSitemap_addChild(t *testing.T) {
 				},
 			},
 			want: true,
+		},
+		{
+			name: "Error on adding one child to Sitemap top-level",
+			p: &Sitemap{
+				{
+					ID:       1,
+					Title:    "Some title 1",
+					Path:     "/some/path1",
+					Priority: 1,
+				},
+				{
+					ID:       2,
+					Title:    "Some title 2",
+					Path:     "/some/path2",
+					Priority: 1,
+				},
+			},
+			args: args{
+				parentID: 4,
+				child: &SitemapPath{
+					ID:       3,
+					ParentID: 4,
+					Title:    "Second child title",
+					Path:     "/child/path",
+					Priority: 0.8,
+				},
+			},
+			want: false,
 		},
 	}
 
@@ -402,9 +497,51 @@ func Test_IsEmptyStruct(t *testing.T) {
 		want bool
 	}{
 		{
-			name: "Test empty SitemapPath struct to true",
+			name: "Test nil to true",
 			args: args{
-				object: SitemapPath{},
+				object: nil,
+			},
+			want: true,
+		},
+		{
+			name: "Test empty bool to true",
+			args: args{
+				object: false,
+			},
+			want: true,
+		},
+		{
+			name: "Test empty string to true",
+			args: args{
+				object: "",
+			},
+			want: true,
+		},
+		{
+			name: "Test pointer to empty string to true",
+			args: args{
+				object: new(string),
+			},
+			want: true,
+		},
+		{
+			name: "Test non-empty string to false",
+			args: args{
+				object: "Some",
+			},
+			want: false,
+		},
+		{
+			name: "Test pointer to non-empty slice of int to false",
+			args: args{
+				object: &[1]int{1},
+			},
+			want: false,
+		},
+		{
+			name: "Test empty Sitemap struct to true",
+			args: args{
+				object: Sitemap{},
 			},
 			want: true,
 		},
@@ -414,6 +551,95 @@ func Test_IsEmptyStruct(t *testing.T) {
 				object: &Sitemap{},
 			},
 			want: true,
+		},
+		{
+			name: "Test non-empty Sitemap struct to false",
+			args: args{
+				object: Sitemap{
+					{
+						ID:       1,
+						Title:    "Some title",
+						Path:     "/some/path",
+						Priority: 1,
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Test pointer to non-empty Sitemap struct to false",
+			args: args{
+				object: &Sitemap{
+					{
+						ID:       1,
+						Title:    "Some title",
+						Path:     "/some/path",
+						Priority: 1,
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Test empty SitemapPath struct to true",
+			args: args{
+				object: SitemapPath{},
+			},
+			want: true,
+		},
+		{
+			name: "Test pointer to empty SitemapPath struct to true",
+			args: args{
+				object: &SitemapPath{},
+			},
+			want: true,
+		},
+		{
+			name: "Test non-empty SitemapPath struct to false",
+			args: args{
+				object: SitemapPath{
+					ID:       1,
+					Title:    "Some title",
+					Path:     "/some/path",
+					Priority: 1,
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Test pointer to non-empty SitemapPath struct to false",
+			args: args{
+				object: &SitemapPath{
+					ID:       1,
+					Title:    "Some title",
+					Path:     "/some/path",
+					Priority: 1,
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Test empty URLParams map to true",
+			args: args{
+				object: URLParams{},
+			},
+			want: true,
+		},
+		{
+			name: "Test pointer to empty URLParams map to true",
+			args: args{
+				object: &URLParams{},
+			},
+			want: true,
+		},
+		{
+			name: "Test non-empty URLParams map to false",
+			args: args{
+				object: URLParams{
+					"param1": 0,
+				},
+			},
+			want: false,
 		},
 		{
 			name: "Test pointer to non-empty URLParams map to false",
