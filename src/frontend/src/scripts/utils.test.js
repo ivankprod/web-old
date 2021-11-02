15a7b2347d77b9f953a116a701e9297dd5be3c16
ivankprod <houseprotector@mail.ru>
2021-11-02 03:46:05 +0300

    Frontend: testing for utils module

--- src/frontend/src/scripts/utils.test.js
+++ src/frontend/src/scripts/utils.test.js
@@ -1,5 +1,29 @@
 import utils from "./utils.js"
 
+afterEach(() => {
+	jest.useRealTimers();
+});
+
+test('utils.sleep to be called', () => {
+	jest.useFakeTimers();
+	jest.spyOn(global, 'setTimeout');
+
+	const callback = jest.fn();
+
+	utils.sleep(10).then(() => { callback(); });
+	return expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 10);
+});
+
+describe('utils.onScrollPB', () => {
+	test('to position absolute', () => {
+		document.body.innerHTML = '<div id="progress-bar"></div>';
+
+		utils.onScrollPB();
+
+		expect(document.getElementById('progress-bar').style.position).toEqual('absolute');
+	});
+});
+
 test('utils.rewriteMetas to throw', () => {
 	expect(utils.rewriteMetas).toThrow("Options not specified!");
 });
