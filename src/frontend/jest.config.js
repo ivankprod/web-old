b094dd799ef4b2bd708a7335f520c852c23b9f14
ivankprod <houseprotector@mail.ru>
2022-05-01 00:39:50 +0500

    test(frontend): fix jest babel-jest root option

--- src/frontend/jest.config.js
+++ src/frontend/jest.config.js
@@ -173,7 +173,7 @@ module.exports = {
 
 	// A map from regular expressions to paths to transformers
 	transform: {
-		'^.+\\.js?$': "babel-jest"
+		"^.+\\.js?$": ["babel-jest", { root: '' }]
 	},
 
 	// An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
